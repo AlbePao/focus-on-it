9402574f42aaac3a1ecfa839d5d54a65d399e6e2
Alberto Paolucci <alberto.paolucci@icloud.com>
2021-03-17 22:40:05 +0100

    Import required mat modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,30 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { InfoComponent } from './views/info/info.component';
 import { SettingsComponent } from './views/settings/settings.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    InfoComponent,
-    SettingsComponent
-  ],
+  declarations: [AppComponent, HomeComponent, InfoComponent, SettingsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule,
+    RouterModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
