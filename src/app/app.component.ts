043379377f32d5eeb95a3f829fdb9792a132d101
Alberto Paolucci <alberto.paolucci@icloud.com>
2021-03-21 23:14:40 +0100

    Navigation animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import browser from 'browser-detect';
+import { AnimationsService } from './services/animations/animations.service';
+import { routeAnimations } from './services/animations/route.animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [routeAnimations],
 })
-export class AppComponent {
-  title = 'focus-on-it';
+export class AppComponent implements OnInit {
+  constructor(private readonly animationsService: AnimationsService) {}
+
+  ngOnInit(): void {
+    const isIEorEdgeOrSafari = ['ie', 'edge', 'safari'].includes(browser().name ?? '');
+
+    this.animationsService.updateRouteAnimationType(!isIEorEdgeOrSafari, true);
+  }
 }
