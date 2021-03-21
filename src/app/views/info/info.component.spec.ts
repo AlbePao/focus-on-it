a18866a730aaf883d58960dbc26ba5986e051ad4
Alberto Paolucci <alberto.paolucci@icloud.com>
2021-03-21 22:45:58 +0100

    Info component UI

--- src/app/views/info/info.component.spec.ts
+++ src/app/views/info/info.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { InfoComponent } from './info.component';
 
 describe('InfoComponent', () => {
@@ -8,9 +7,8 @@ describe('InfoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InfoComponent ]
-    })
-    .compileComponents();
+      declarations: [InfoComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
