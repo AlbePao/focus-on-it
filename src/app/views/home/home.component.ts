d2a2efa92a37e7ecd9e6941dbfa0c464fd98784a
Alberto Paolucci <alberto.paolucci@icloud.com>
2021-04-10 15:22:46 +0200

    Dialogs dimensions

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -149,9 +149,8 @@ export class HomeComponent implements OnInit, OnDestroy {
       this.dialog
         .open(DialogAlertComponent, {
           disableClose: true,
-          width: '50%',
+          width: '20rem',
           height: 'auto',
-          maxWidth: '40rem',
           data: {
             title: 'Warning',
             description: 'Are you sure you want to stop timer? All your progress will be lost',
@@ -167,9 +166,8 @@ export class HomeComponent implements OnInit, OnDestroy {
       this.dialog
         .open(DialogAlertComponent, {
           disableClose: true,
-          width: '50%',
+          width: '20rem',
           height: 'auto',
-          maxWidth: '20rem',
           data: { description: 'Work completed' },
         })
         .afterClosed(),
