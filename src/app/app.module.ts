5368ed95b695ae34c4ca812339e9dca2f501bebf
Alberto Paolucci <alberto.paolucci@icloud.com>
2021-04-11 16:01:19 +0200

    Add @angular/pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 // TODO: add ngx-translate
 @NgModule({
@@ -21,6 +23,12 @@ import { AppComponent } from './app.component';
     MatIconModule,
     MatToolbarModule,
     RouterModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
