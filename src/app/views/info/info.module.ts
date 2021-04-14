0f31b10728a0c07902489547fbeeef1a8ff2d6c3
Alberto Paolucci <alberto.paolucci@icloud.com>
2021-04-14 21:31:06 +0200

    Info page translations

--- src/app/views/info/info.module.ts
+++ src/app/views/info/info.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { TranslateModule } from '@ngx-translate/core';
 import { InfoRoutingModule } from './info-routing.module';
 import { InfoComponent } from './info.component';
 
 @NgModule({
   declarations: [InfoComponent],
-  imports: [CommonModule, InfoRoutingModule, TranslateModule],
+  imports: [CommonModule, InfoRoutingModule, MatButtonModule, TranslateModule],
 })
 export class InfoModule {}
